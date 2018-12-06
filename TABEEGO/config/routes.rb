22457933f6f47c059084dcba321819675d538824
TAIKI <extra.experiority@gmail.com>
2018-12-07 01:30:01 +0900

    [update] top/index の html 追加

--- TABEEGO/config/routes.rb
+++ TABEEGO/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top/index' 
+  get 'top/index'
   get "area/index"
   get "profile/index"
   get "questions/index"
