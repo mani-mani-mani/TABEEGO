2225edfaad2cad97459b10535c885fbdd23cf359
Riki <ricky1231.tennis.math@gmail.com>
2018-12-21 19:20:17 +0900

    [add]プロフィールページ追加

--- TABEEGO/config/routes.rb
+++ TABEEGO/config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "top#index"
-  get "admin", to: "top#admin"
+  get "/admin", to: "top#admin"
 
-  get "login", to: "top#login"
-  post "login", to:"top#login_check"
+  get "/login", to: "top#login"
+  post "/login", to:"top#login_check"
 
-  get "register", to:"top#register"
-  post "register", to:"top#register_user"
+  get "/register", to:"top#register"
+  post "/register", to:"top#register_user"
 
+  get "/profile", to: "profile#index"
 end
 
