5c9f4bf1386f15596b7f5d1a303d7d9127f50589
Riki <ricky1231.tennis.math@gmail.com>
2018-12-21 19:10:31 +0900

    [add]登録・ログイン機能の追加

--- TABEEGO/app/controllers/top_controller.rb
+++ TABEEGO/app/controllers/top_controller.rb
@@ -1,4 +1,45 @@
 class TopController < ApplicationController
     def index
     end
+
+    def admin
+        @users = User.all
+    end
+
+    def register
+    end
+
+    def register_user
+        username = params[:username]
+        password = params[:password]
+
+        user = User.new
+        user.name = username
+        user.password = password
+        user.save
+
+        redirect_to "/login"
+    end
+
+    def login
+    end
+
+    def login_check
+        username = params[:username]
+        password = params[:password]
+
+        users = User.where("name = ? and password = ?", username, password)
+        user = users[0];
+
+        if user
+            session[:id] = user.id;
+            redirect_to "/"
+        else
+            redirect_to "/login", flash:{ error_message: "ユーザー名かパスワードが間違っています。"}
+        end
+
+    end
+    
+    def logout
+    end
 end
--- TABEEGO/config/routes.rb
+++ TABEEGO/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top/index'
-  get "area/index"
-  get "profile/index"
-  get "questions/index"
+  get "/", to: "top#index"
+  get "admin", to: "top#admin"
+
+  get "login", to: "top#login"
+  post "login", to:"top#login_check"
+
+  get "register", to:"top#register"
+  post "register", to:"top#register_user"
 
 end
 
